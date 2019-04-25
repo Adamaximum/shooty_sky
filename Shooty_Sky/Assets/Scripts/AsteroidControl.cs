413a602a46a2718e9155bfd48769f783b3abe917
Adam Goren <amg1090@nyu.edu>
2019-04-25 11:41:23 -0400

    Added entire project

---
+++ Shooty_Sky/Assets/Scripts/AsteroidControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidControl : MonoBehaviour {
+
+    public GameManager gm;
+    public PlayerControl player;
+
+    //public AudioSource astExpSnd;
+
+    public GameObject explosionPrefab;
+    public GameObject shipExpPrefab;
+    
+	// Use this for initialization
+	void Start () {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        player = GameObject.Find("Player").GetComponent<PlayerControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            //Assigns explosion animation to player and ends the game
+            gm.gameState = 3;
+            GameObject shipExp = Instantiate(shipExpPrefab);
+            shipExp.transform.position = player.transform.position;
+        }
+
+        if(collision.gameObject.tag == "bullet")
+        {
+            //Assigns explosion animation to an asteroid and destroys it
+            GameObject explosion = Instantiate(explosionPrefab);
+            explosion.transform.position = transform.position;
+
+            int index = gm.asteroids.IndexOf(gameObject);
+            gm.asteroids.Remove(gameObject);
+            gm.astSpeed.RemoveAt(index);
+            gm.astArt.RemoveAt(index);
+            Debug.Log("Destroying Asteroid");
+
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+
+            gm.astDest++;
+
+        }
+    }
+}
