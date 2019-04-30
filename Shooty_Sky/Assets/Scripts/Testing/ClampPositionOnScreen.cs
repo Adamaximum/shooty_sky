57eaa723a5f46fe8679287cfae799cc3d9089dde
Zhou Fang <zf472@nyu.edu>
2019-04-30 10:59:41 -0400

    More codes

--- Shooty_Sky/Assets/Scripts/Testing/ClampPositionOnScreen.cs
+++ Shooty_Sky/Assets/Scripts/Testing/ClampPositionOnScreen.cs
@@ -5,16 +5,18 @@ using UnityEngine.Rendering;
 
 public class ClampPositionOnScreen : MonoBehaviour
 {
+    public Vector2 size;
+    
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         var position = transform.position;
         var posX = position.x;
         var posY = position.y;
-        posX = Mathf.Clamp(posX, Camera.main.ScreenToWorldPoint(Vector2.zero).x,
-            Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).x);
-        posY = Mathf.Clamp(posY, Camera.main.ScreenToWorldPoint(Vector2.zero).y,
-            Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).y);
+        posX = Mathf.Clamp(posX, Camera.main.ScreenToWorldPoint(Vector2.zero).x + size.x / 2,
+            Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).x - size.x / 2);
+        posY = Mathf.Clamp(posY, Camera.main.ScreenToWorldPoint(Vector2.zero).y + size.y / 2,
+            Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).y - size.y / 2);
         position = new Vector2(posX, posY);
         transform.position = position;
     }
--- Shooty_Sky/Assets/Scripts/Testing/MovementByInput.cs
+++ Shooty_Sky/Assets/Scripts/Testing/MovementByInput.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MovementByInput : Movement
 {
     // Start is called before the first frame update
-    void Update()
+    private void FixedUpdate()
     {
         transform.position += InputMovement();
     }
