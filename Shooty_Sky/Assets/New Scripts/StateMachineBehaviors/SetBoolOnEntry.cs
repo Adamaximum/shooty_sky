c4f59c3e8af216db9870f2d6ce5bc0290cce0379
Zhou Fang <zf472@nyu.edu>
2019-05-07 00:32:55 -0400

    more content
    enemy1 movement needs to be more advanced

---
+++ Shooty_Sky/Assets/New Scripts/StateMachineBehaviors/SetBoolOnEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetBoolOnEntry : StateMachineBehaviour
+{
+    public string name;
+
+    public bool value;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(name, value);
+    }
+}
