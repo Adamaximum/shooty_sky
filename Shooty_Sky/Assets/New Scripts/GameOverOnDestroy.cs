c696b37353c9ea284f769729852322c939bbb9de
Zhou Fang <zf472@nyu.edu>
2019-05-09 08:32:53 -0400

    game over

---
+++ Shooty_Sky/Assets/New Scripts/GameOverOnDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverOnDestroy : MonoBehaviour
+{
+    public GameMasterTracker tracker;
+
+    private void OnDestroy()
+    {
+        tracker.gameManager.gameState = 11;
+    }
+}
