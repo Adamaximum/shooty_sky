9d4a02adccefa2f13784bfee8df59ba99b6e82fc
Zhou Fang <zf472@nyu.edu>
2019-05-09 05:52:30 -0400

    Changes

---
+++ Shooty_Sky/Assets/DelayState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayState : StateMachineBehaviour
+{
+    public float delayTime;
+    private float _currentTime;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _currentTime = 0;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _currentTime += Time.deltaTime;
+        if (_currentTime >= delayTime)
+        {
+            animator.SetBool("Delayed", true);
+        }
+    }
+}
